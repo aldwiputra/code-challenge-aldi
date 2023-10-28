20607fd2c33b3d3f2362e48ba663d0bb9c6d66a5
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-10-28 17:04:38 +0800

    colorize device type

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -14,18 +14,29 @@ export function TableRow({ data }: TableRowPropsType) {
       onClick={() => {
         navigate(`/${deviceId}`);
       }}
-      className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-600'>
+      className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-50 dark:hover:bg-slate-700'>
       <td
         scope='row'
         className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
         {deviceId}
       </td>
-      <td className='px-6 py-4'>{deviceType}</td>
+      <td className='px-6 py-4'>
+        <span
+          className={`text-sm font-medium mr-2 px-2.5 py-0.5 rounded ${
+            deviceType === 'Aircraft'
+              ? 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300'
+              : deviceType === 'Personal'
+              ? 'bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-300'
+              : 'bg-red-100 text-red-800  dark:bg-red-900 dark:text-red-300'
+          }`}>
+          {deviceType}
+        </span>
+      </td>
       <td className='px-6 py-4 text-gray-900 dark:text-slate-300'>
         {formatDate(timestamp)}{' '}
-        <span className='text-xs bg-zinc-500/30 py-1 px-1.5 rounded-md text-gray-900 dark:text-slate-200'>
+        <span className='text-xs bg-green-500/20 ml-1.5 py-1 px-1.5 rounded-md text-gray-900 dark:text-slate-200'>
           {formatTime(timestamp)}
-        </span>{' '}
+        </span>
       </td>
       <td className='px-6 py-4 text-center text-gray-900 dark:text-white'>{location}</td>
     </tr>
